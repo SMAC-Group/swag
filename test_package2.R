acef361578e8fafcffc883d38e4e22f2832e388a
Cesare Miglioli <cesare.miglioli@hotmail.it>
2020-06-04 12:16:50 +0200

    small modif. to test package 2

--- test_package2.R
+++ test_package2.R
@@ -18,15 +18,8 @@ learner <- caret::train(as.data.frame(x$Cl.thickness),y,method="knn",preProcess=
 # test_lasso <- swag(y, X, learner = "lasso", dmax = 3, q0 = .5, nc = 4, verbose = TRUE)
 # test_rf <- swag(y, X, learner = "rf", dmax = 3, q0 = .5, nc = 4, verbose = TRUE)
 
-# @Cesare: please use devtools::install_github("SMAC-Group/SWAG-R-Package")
-# setwd("C:/Users/Cesare/Desktop/NIPS/SWAG-R-Package/R")
-#
-# source("swag.R")
-#
-# source("control.R")
 
-
-trial <- swagControl(pmax = 3,alpha = 0.05,m = 100,seed = 163L, verbose = FALSE)
+trial <- swagControl(pmax = 3,alpha = 0.3,m = 100,seed = 163L, verbose = FALSE)
 
 # error: Error in structure(pmax = pmax, m = m, alpha = alpha, seed = seed, class = "swagControl") : argument ".Data" is missing, with no default
 
@@ -34,13 +27,20 @@ trial_2 <- auto_swagControl(x = x,y = y,control = swagControl()) #not clear cont
 
 metric <- "Accuracy"
 
+require(caret)
+
 trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 1)
 
+try_obj <- swag(x = x, y = y,control = trial, auto_control = F,method = "svmLinear", metric = metric, trControl=trctrl, preProcess = c("center", "scale"),tuneLength = 10 )
 
-try_obj <- swag(x = as.matrix(x), y = y,control = trial,auto_control = F,method = "svmLinear", metric = metric, trControl=trctrl, preProcess = c("center", "scale"),tuneLength = 10)
+#try_obj <- swag(x = as.matrix(x), y = y,control = trial,auto_control = F,method = "svmLinear", metric = metric, trControl=trctrl, preProcess = c("center", "scale"),tuneLength = 10)
 
-try_obj <- swag(x = as.matrix(x), y = y,auto_control = T,method = "svmLinear", metric = metric, trControl=trctrl, preProcess = c("center", "scale"),tuneLength = 10)
+#try_obj <- swag(x = as.matrix(x), y = y,auto_control = T,method = "svmLinear", metric = metric, trControl=trctrl, preProcess = c("center", "scale"),tuneLength = 10)
+
 
 # Update sam:
 
 fit_swag <- swag(x = x, y = y, control=swagControl(alpha=.5), method = "svmLinear", metric = metric, trControl=trctrl, preProcess = c("center", "scale"),tuneLength = 10)
+
+# Error in if (control$verbose) print(paste0("Dimension explored: ", 1,  :argument is of length zero
+
